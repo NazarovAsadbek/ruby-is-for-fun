20fcd0b56296d642beae048534844c2fab7ea206
NazarovAsadbek <nazarov_asadbek_official@mail.ru>
2022-06-12 23:22:08 +0500

    puts && gets
    Roman Pushkin. Ruby is for fun.
    Page 41/288.

---
+++ app.rb
@@ -0,0 +1,26 @@
+# puts "I would hug you, but I’m just a text"
+# gets
+
+# puts "How to translate ball?"
+# gets
+# puts "How to translate door?"
+# gets
+# puts "How to translate peace?"
+# gets
+
+
+# puts "Your date of birthday"
+# date_of_birth = gets
+#
+# puts "Your place of birthday"
+# place_of_birth = gets
+#
+# puts "Phone number three clients"
+# phone_number = gets
+#
+# puts "Year of birth = " + date_of_birth + "place of birth = " + place_of_birth + "Phone number three clients = " + phone_number
+
+puts "Your age?"
+age = gets
+puts "=" * 80
+puts "Your age is " + age
