6c5e0c91c3b399bb412d065bf9c82a1fa7737245
NazarovAsadbek <nazarov_asadbek_official@mail.ru>
2022-06-15 00:05:28 +0500

    Last page: 49

--- app.rb
+++ app.rb
@@ -8,7 +8,6 @@
 # puts "How to translate peace?"
 # gets
 
-
 # puts "Your date of birthday"
 # date_of_birth = gets
 #
@@ -20,7 +19,35 @@
 #
 # puts "Year of birth = " + date_of_birth + "place of birth = " + place_of_birth + "Phone number three clients = " + phone_number
 
-puts "Your age?"
-age = gets
-puts "=" * 80
-puts "Your age is " + age
+# puts "Your age?"
+# age = gets
+# puts "=" * 80
+# puts "Your age is " + age
+#
+
+# puts "This is string".class
+#
+# puts 42.class
+#
+# puts "".class
+#
+# puts 0.class
+#
+# puts -1.class
+#
+# puts 3.14.class
+
+# puts 123.class.class
+
+# puts 123.is_a?(Object)
+
+# puts "Your age?"
+# age = gets
+# age_month = age.to_i * 12
+# puts "Your age is " + age_month.to_s
+
+puts "Your salary?"
+salary = gets.to_i
+tax_rate = 0.3
+puts "Tax:"
+puts salary * tax_rate
